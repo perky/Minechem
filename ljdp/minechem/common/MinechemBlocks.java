5d4de1da6cc302ac2489ba6743b085f2b1fde057
Rushmead <Rushmead@techie.com>
2012-12-17 17:08:23 +0000

    Added the Config(HopeFully)

--- ljdp/minechem/common/MinechemBlocks.java
+++ ljdp/minechem/common/MinechemBlocks.java
@@ -3,16 +3,16 @@ package ljdp.minechem.common;
 import net.minecraft.block.Block;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
-
+import ljdp.minechem.common.ModMineChem;
 public class MinechemBlocks {
 	public static Block decomposer;
 	public static Block microscope;
 	public static Block synthesis;
 	
 	public static void registerBlocks() {
-		decomposer = new BlockDecomposer(4011);
-		microscope = new BlockMicroscope(4012);
-		synthesis  = new BlockSynthesis(4013);
+		decomposer = new BlockDecomposer(decomposerID);
+		microscope = new BlockMicroscope(microscopeID);
+		synthesis  = new BlockSynthesis(synthesisID);
 		
 		GameRegistry.registerBlock(decomposer);
 		GameRegistry.registerTileEntity(TileEntityDecomposer.class, "MinechemTileEntityDecomposer");
