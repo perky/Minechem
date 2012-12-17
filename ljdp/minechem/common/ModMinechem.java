3139e7dd286955477b892cf343c6cbca91bb962b
Rushmead <Rushmead@techie.com>
2012-12-17 17:06:30 +0000

    Added the Config(HopeFully)

--- ljdp/minechem/common/ModMinechem.java
+++ ljdp/minechem/common/ModMinechem.java
@@ -36,24 +36,50 @@ public class ModMinechem implements IGuiHandler {
 	
 	@SidedProxy(clientSide="ljdp.minechem.client.ClientProxy", serverSide="ljdp.minechem.common.CommonProxy")
 	public static CommonProxy proxy;
-	
+	public static int elementID;
+	public static int moleculeID;
+	public static int decomposerID;
+	public static int microscopeID;
+	public static int synthesisID;
+	public static Logger blLog = Logger.getLogger("MineChem");
 	public static CreativeTabs minechemTab = new CreativeTabMinechem("mineChem");
 	
 	@PreInit
 	public void preInit(FMLPreInitializationEvent event) {
 		MinecraftForge.EVENT_BUS.register(MinechemRecipes.getInstance());
+		blLog.setParent(FMLLog.getLogger());
+
+	 	blLog.info(Loading Config);
+	 	loadConfig(event);
 	}
 	
 	@Init
 	public void init(FMLInitializationEvent event) {
+		blLog.info("Adding Items");
 		MinechemItems.registerItems();
+		blLog.info("Adding Blocks");
 		MinechemBlocks.registerBlocks();
+		blLog.info("Adding Recipes");
 		MinechemRecipes.getInstance().RegisterRecipes();
+		
 		NetworkRegistry.instance().registerGuiHandler(this, this);
 		proxy.registerRenderers();
 		LanguageRegistry.instance().addStringLocalization("itemGroup.mineChem", "en_US", "MineChem");
 	}
-	
+	public void loadConfig(FMLPreInitializationEvent event){
+	 	Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+
+	 	//Items go below here(I put in an example)
+	 	//obbySwordID = config.getItem(config.CATEGORY_ITEM,"Obsidian Sword", 509).getInt(509);
+	 	//Blocks go below here(Another Example)
+	 	//oreObbyID = config.get(config.CATEGORY_BLOCK,"Obsidian Ore", 201).getInt(201);
+	 	elementID = config.getItem(config.CATEGORY_ITEM,"Element", 4736).getInt(4736);
+	 	moleculeID = config.getItem(config.CATEGORY_ITEM,"MoleCule", 4737).getInt(4737);
+	 	microscopeID = config.get(config.CATEGORY_BLOCK,"Microscope", 4012).getInt(4012);
+	 	decomposerID = config.get(config.CATEGORY_BLOCK,"Decomposer", 4011).getInt(4011);
+	 	synthesisID = config.get(config.CATEGORY_BLOCK,"Synthesis", 4013).getInt(4013);
+	 	config.save();
+	 }
 	@PostInit
 	public void postInit(FMLPostInitializationEvent event) {
 		
