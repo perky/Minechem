b30475b40eb4f6fe5825915c5edd6718d3940526
Luke <luke.perkin@gmail.com>
2012-12-31 11:09:33 +0000

    Move guiHandler to its own class

---
+++ ljdp/minechem/common/GuiHandler.java
@@ -0,0 +1,77 @@
+package ljdp.minechem.common;
+
+import ljdp.minechem.client.gui.GuiDecomposer;
+import ljdp.minechem.client.gui.GuiFusion;
+import ljdp.minechem.client.gui.GuiMicroscope;
+import ljdp.minechem.client.gui.GuiProjector;
+import ljdp.minechem.client.gui.GuiSynthesis;
+import ljdp.minechem.common.containers.ContainerDecomposer;
+import ljdp.minechem.common.containers.ContainerFusion;
+import ljdp.minechem.common.containers.ContainerMicroscope;
+import ljdp.minechem.common.containers.ContainerProjector;
+import ljdp.minechem.common.containers.ContainerSynthesis;
+import ljdp.minechem.common.tileentity.TileEntityBlueprintProjector;
+import ljdp.minechem.common.tileentity.TileEntityDecomposer;
+import ljdp.minechem.common.tileentity.TileEntityFusion;
+import ljdp.minechem.common.tileentity.TileEntityMicroscope;
+import ljdp.minechem.common.tileentity.TileEntityProxy;
+import ljdp.minechem.common.tileentity.TileEntitySynthesis;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+import cpw.mods.fml.common.network.IGuiHandler;
+
+public class GuiHandler implements IGuiHandler {
+	@Override
+	public Object getServerGuiElement(int ID, EntityPlayer player, World world,
+			int x, int y, int z) {
+		TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
+		if(tileEntity instanceof TileEntityDecomposer)
+			return new ContainerDecomposer(player.inventory, (TileEntityDecomposer)tileEntity);
+		if(tileEntity instanceof TileEntityMicroscope)
+			return new ContainerMicroscope(player.inventory, (TileEntityMicroscope)tileEntity);
+		if(tileEntity instanceof TileEntitySynthesis)
+			return new ContainerSynthesis(player.inventory, (TileEntitySynthesis)tileEntity);
+		if(tileEntity instanceof TileEntityFusion)
+			return new ContainerFusion(player.inventory, (TileEntityFusion)tileEntity);
+		if(tileEntity instanceof TileEntityProxy)
+			return getServerGuiElementFromProxy((TileEntityProxy)tileEntity, player);
+		if(tileEntity instanceof TileEntityBlueprintProjector)
+			return new ContainerProjector(player.inventory, (TileEntityBlueprintProjector)tileEntity);
+		return null;
+	}
+
+	public Object getServerGuiElementFromProxy(TileEntityProxy proxy, EntityPlayer player) {
+		TileEntity tileEntity = proxy.getManager();
+		if(tileEntity instanceof TileEntityFusion)
+			return new ContainerFusion(player.inventory, (TileEntityFusion)tileEntity);
+		return null;
+	}
+	
+
+	@Override
+	public Object getClientGuiElement(int ID, EntityPlayer player, World world,
+			int x, int y, int z) {
+		TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
+		if(tileEntity instanceof TileEntityDecomposer)
+			return new GuiDecomposer(player.inventory, (TileEntityDecomposer)tileEntity);
+		if(tileEntity instanceof TileEntityMicroscope)
+			return new GuiMicroscope(player.inventory, (TileEntityMicroscope)tileEntity);
+		if(tileEntity instanceof TileEntitySynthesis)
+			return new GuiSynthesis(player.inventory, (TileEntitySynthesis)tileEntity);
+		if(tileEntity instanceof TileEntityFusion)
+			return new GuiFusion(player.inventory, (TileEntityFusion)tileEntity);
+		if(tileEntity instanceof TileEntityProxy)
+			return getClientGuiElementFromProxy((TileEntityProxy)tileEntity, player);
+		if(tileEntity instanceof TileEntityBlueprintProjector)
+			return new GuiProjector(player.inventory, (TileEntityBlueprintProjector)tileEntity);
+		return null;
+	}
+	
+	public Object getClientGuiElementFromProxy(TileEntityProxy proxy, EntityPlayer player) {
+		TileEntity tileEntity = proxy.getManager();
+		if(tileEntity instanceof TileEntityFusion)
+			return new GuiFusion(player.inventory, (TileEntityFusion)tileEntity);
+		return null;
+	}
+}
