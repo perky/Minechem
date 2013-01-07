9de05b9598cf0ceafa317bfe51ee49600c80db17
MFernflower <machacker9000@gmail.com>
2013-01-06 21:58:51 -0500

    Update ljdp/minechem/api/core/EnumMolecule.java

--- ljdp/minechem/api/core/EnumMolecule.java
+++ ljdp/minechem/api/core/EnumMolecule.java
@@ -114,7 +114,7 @@ public enum EnumMolecule {
 	salt 					(89, "Sodium Chloride", new Element(Na,1), new Element(Cl,1)), 
 	nh3  					(90, "Aqueous Ammonia", new Element(N,1), new Element(H,4), new Molecule(hydroxide)),
 	nod 					(91, "Nodularin", new Element(C,41), new Element(H,60), new Element(N,8), new Element(O,10)),
-	becauseImAPotato                        (92, "Digoxin", new Element(C,41), new Element(H,64), new Element(O,14)), // Will come from bad potatos
+	potato                       (92, "Digoxin", new Element(C,41), new Element(H,64), new Element(O,14)), // Will come from bad potatos
 	ttx                                     (93, "TTX - Tetrodotoxin", new Element(C,11), new Element(H,11), new Element(N,3),new Element(O,8)), // Could come from spider eyes
 	;
 	
